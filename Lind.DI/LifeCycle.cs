105d03bf996de5e74687f6104cbdcea25e9870a9
lind <bfyxzls@sina.com>
2019-04-21 14:36:16 +0800

    添加说明

--- Lind.DI/LifeCycle.cs
+++ Lind.DI/LifeCycle.cs
@@ -9,8 +9,17 @@ namespace Lind.DI
     /// </summary>
     public enum LifeCycle
     {
+        /// <summary>
+        /// 瞬息对象，使用完就释放
+        /// </summary>
         CurrentScope,
+        /// <summary>
+        /// 当前请求单例对像，当前http请求结束后，自动释放
+        /// </summary>
         CurrentRequest,
+        /// <summary>
+        /// 全局的单例对象，当应用程序重新启动时，自动创建，应用程序进程销毁后，它自动释放
+        /// </summary>
         Global,
     }
 }
---
+++ Lind.DI/ReadMe.md
@@ -0,0 +1,20 @@
+﻿# Lind.DI的意义
+改善Ioc注入的方式，主要是借鉴了java spring框架，觉得它的注释方式实现的IOC注入是比较方便的。
+
+# Lind.DI主要依赖的项目
+* Autofac
+* Autofac.Extras.DynamicProxy
+
+# 特性（attribute）的介绍
+* Component	添加在类上，这个类会被注册到IOC容器里
+* Injection	添加在字段上，这个字段会从IOC容器里注入对应的实例
+
+# 对象的生命周期
+* CurrentScope
+* CurrentRequest
+* Global
+
+# Lind.DI使用
+* 全局注册所有组件        DIFactory.Init();
+* 拦截当前对象，并注入    DIFactory.InjectFromObject(this)
+
