3181f709c1ace33d51d3442708506c6d8f2b2082
zhangzl <zhangzl@i-counting.cn>
2019-04-22 10:26:00 +0800

    添加了测试项目

---
+++ Lind.DI.Api/Filters/DIFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Lind.DI.Api.Filters
+{
+	public class DIFilter : ActionFilterAttribute
+	{
+        public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			Lind.DI.DIFactory.InjectFromObject(context.Controller);
+		}
+	}
+}
